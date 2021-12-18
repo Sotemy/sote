5ab517afc73060deda9f711d410e631295c64c04
moninger <moninger76131@gmail.com>
2021-12-18 15:37:09 +0100

    rewrote main.js to work with registration modal

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,20 +1,25 @@
-
-// $(document).ready(function(){
-//     $('.modal-footer btn-primary').on('click', function(){
-//         let login=$('#loginInput').val();
-//         let password=$('#passInput').val();
-
-//         req = $.ajax({
-//             url:'/auth/login',
-//             type:'POST',
-//             data:{login:login, password:password}
-//         })
-//     });
-// });
-
 $(function () {
-    $("#logbtn").on('click', function () {
-        $("#myModal").modal("show", { keyboard: true });
+
+    $('.nav-item dropdown').on('click', function(){
+        $(".dropdown-menu").animate({
+            height: 'toggle'
+        });
+    });
+    
+    $(".userbtn").on('click', function () {
+        console.log($(this).text() == 'register');
+        $('.modal-title').text($(this).text());
+        if($(this).text() === 'register'){
+            console.log($(this).text() == 'register' && $('#passwordInput2'));
+            console.log($('#passwordInput2'));
+            console.log($('#passwordInput2').length);
+            if ($('#passwordInput2').length == 0){
+                $('#userForm').append("<div class='mb-3' id='mb-2'><label for='pwd' class='form-label'>Repeat password:</label><input type='password' class='form-control' id='passwordInput2' placeholder='Repeat password' name='pswd' required></div>");
+            };
+        } else if ($(this).text() === 'login'){
+            $('#mb-2').remove();
+        }
+        return $("#myModal").modal("show", { keyboard: true });
 
     });
 
@@ -22,35 +27,53 @@ $(function () {
 
         let login = $('#loginInput');
         let password = $('#passwordInput');
-        let log = `login: ${login.val()}, password: ${password.val()}`;
-        console.log(log);
+        let password2= $('#passwordInput2');
+        let validornot = validateData(login, password, password2);
 
-        let validornot = validateData(login, password);
+        // $('input').focus(function() {
+        //     $( this ).next( validornot );
+        // });
 
-        console.log(validornot);
+        if (validornot === true) {
+            return $('.form-control').attr("class", "form-control is-invalid");
+        };
 
-        if (validornot === null) {
-            $('.form-control').attr("class", "form-control is-valid");
+        
+
+        $('.form-control').attr("class", "form-control is-valid");
+        if (password2) {
             req=$.ajax({
                 type: "POST",
-                url: '/auth/login',
-                data: { login: login.val(), password: password.val() },
+                url: '/auth/register',
+                data: { login: login.val(), password: password.val(), password2:password2.val()},
                 success: function (text) {
                     let response = text;
-                    console.log(response);
+                    return response;
                 },
-                error: function (xhr, status, error) {
-
-                    return console.log(status, error, );
-                }//'\n\nError:' + xhr.responseText
+                error: function (status, error) {
+    
+                    return status, error;
+                }
             });
-            req.done(function(){
+        };
 
-            });
-        } else {
-            $(validornot).attr("class", "form-control is-invalid");
-            console.log('problem with creditals');
-        }
+        req=$.ajax({
+            type: "POST",
+            url: '/auth/login',
+            data: { login: login.val(), password: password.val() },
+            success: function (text) {
+                let response = text;
+                return response;
+            },
+            error: function (status, error) {
+
+                return status, error;
+            }
+        });
+
+        req.done(function(){
+            return console.log('1');
+        });
     });
 
     const rule = {
@@ -60,35 +83,39 @@ $(function () {
         password: {
             min_length: 8,
             excepted_symbols: ['ü', 'Ü', 'ö', 'Ö', 'ä', 'Ä']
+        },
+        password2:{
+            equals:$('#passwordInput').val()
         }
     };
 
-    let validateData = function (login, password) {
-        return validateLogin(login), validatePassword(password);
-    };
 
-    // if(login.val().length<rule.login.min_length){
-    //     if (password.val().length<rule.password.min_length) {
-    //         return password
-    //     };
-    //     return login
-    // };
-
-
-    let validatePassword = function (password) {
+    function validatePassword (password) {
         if (password.val().length < rule.password.min_length) {
-            return password
-        } else {
-            return null;
+            return false;
         };
+        return true;
+
     };
 
-    let validateLogin = function (login) {
+    function validateLogin (login) {
         if (login.val().length < rule.login.min_length) {
-            return login
-        } else {
-            return null;
+            return false;
         };
+        return true;
+    };
+
+    function validatePassword2 ( password2){
+        if (password2 != rule.password2.equals){
+            console.log(rule.password2.equals, password2)
+            return false;
+        };
+        return true;
+    };
+
+    function validateData (login, password, password2) {
+        if(password2===null){return validateLogin(login) && validatePassword(password);};
+        return validateLogin(login) && validatePassword(password) && validatePassword2(password2);
     };
 
 });
