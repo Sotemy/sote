01e8da5e0f1aa724586c53246f8f5df0161b0e13
moninger <moninger76131@gmail.com>
2022-01-08 22:48:15 +0100

    .

--- app/__init__.py
+++ app/__init__.py
@@ -4,15 +4,9 @@ from flask_mail import Mail
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_moment import Moment
-from flask_debugtoolbar import DebugToolbarExtension
-from OpenSSL import SSL
-from app.models import Role, User
 
 from config import Development
 
-# context=SSL.Context(SSL.TLSv1_2_METHOD)
-# context.use_certificate_file('docs/cert.pem')
-# context.use_privatekey_file('docs/key.pem')
 
 app=Flask(__name__)
 app.config.from_object(Development)
@@ -21,7 +15,6 @@ login_manager=LoginManager(app)
 mail=Mail(app)
 migrate = Migrate(app, db, render_as_batch=True)
 moment = Moment(app)
-# toolbar = DebugToolbarExtension(app)
 
 from app.main import main
 from app.auth import auth
